3cd466046f1ce4886662493e9d8b73e08e465be2
Aske L Ejdrup <50575240+Ejdrup@users.noreply.github.com>
2024-03-03 16:19:20 +0100

    Add files via upload

---
+++ sim_functions_pr.py
@@ -0,0 +1,233 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 17 10:39:58 2023
+
+@author: ejdrup
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+import scipy.signal as ss
+
+def sim_space_neurons_3D_dynamicPr(width = 100, depth = 10, dx_dy = 1, time = 1, D = 763, 
+              inter_var_distance = 2.92, p_r = [0.06,0.06], f_rate = 4, n_neurons = 150, Hz = 0.01):
+    ## Generate overall simulation space
+    
+    # Simulation time, sec.
+    t = time
+    # Field size, um
+    w = h = width
+    # Depth of field
+    depth = depth
+    # Intervals in x-, y- directions, um
+    dx = dy = dz = dx_dy
+    # Steps per side
+    nx, ny, nz = int(w/dx), int(h/dy), int(depth/dz)
+    # Calculate time step
+    dx2, dy2 = dx*dx, dy*dy
+    dt = dx2 * dy2 / (2 * D * (dx2 + dy2))
+    # Generate simulation space (one snapshot every 10 ms)
+    space0 = np.zeros((int(t/Hz), nx, ny, nz))
+    # Prep for image of all release sites
+    space_ph = np.zeros((nx, ny, nz))
+    
+    ## Generate firing pattern
+    p_r_series = np.interp(np.linspace(0,int(t/dt)-1,int(t/dt)), np.linspace(0,int(t/dt)-1,len(p_r)), p_r)
+    
+    # Number of varicosities
+    n_varico = int((width**2*depth)/inter_var_distance)
+    # Generate varicosities from random linear distribution
+    x_varico = np.random.randint(0, high = (w/dx), size = n_varico)
+    y_varico = np.random.randint(0, high = (w/dy), size = n_varico)
+    z_varico = np.random.randint(0, high = (depth/dz), size = n_varico)
+    
+    # Assign neuron identity to terminals
+    neuro_identity = np.random.randint(0, high = n_neurons, size = n_varico)
+    # Firing pattern of each neuron
+    neuron_firing = np.random.poisson(f_rate*dt,(n_neurons,int(t/dt)))
+    neuron_firing[neuron_firing > 1] = 1 # avoid multiple release events on top of each other
+    
+    # Firing pattern of each terminal
+    firing = neuron_firing[neuro_identity,:]
+    # Add indv release prob of 0.06
+    for i in range(firing.shape[1]):
+        terminal_refraction = np.random.choice(firing.shape[0],int(firing.shape[0]*(1-p_r_series[i])),replace = False)
+        firing[terminal_refraction,i] = 0
+        
+    # terminal_refraction = np.random.poisson(1-p_r,firing.shape)
+    # terminal_refraction[terminal_refraction > 1] = 1 # avoid multiple release events on top of each other
+    # terminal_refraction = (terminal_refraction-1)*-1 # flip logic
+
+    return space0, space_ph, firing.T, np.array([x_varico,y_varico, z_varico]), np.array([time, dt, dx_dy, inter_var_distance, Hz])
+
+def sim_dynamics_3D_dynamicKm(space0, space_ph, release_sites, firing, var_list, 
+                 Q = 3000, uptake_rate = 4*10**-6, Km = [210*10**-9,210*10**-9],
+                 Ds = 321.7237308146399, ECF = 0.21, rev_trans = [0,0], start_occ_D2 = 0.6):
+    # print(uptake_rate)
+    # print(Q)
+    # Extract parameters
+    t = var_list[0]
+    dt = var_list[1]
+    dx_dy = var_list[2]
+    Hz = var_list[4]
+    Kapp = np.interp(np.linspace(0,int(t/dt)-1,int(t/dt)), np.linspace(0,int(t/dt)-1,len(Km)), Km)
+    rev_trans = np.interp(np.linspace(0,int(t/dt)-1,int(t/dt)), np.linspace(0,int(t/dt)-1,len(rev_trans)), rev_trans)
+    
+    # Test for receptor occupany
+    # Affinities D2: https://www.ncbi.nlm.nih.gov/pmc/articles/PMC8071183/
+    occ_D1 = space0.copy()
+    occ_D1_ph = space_ph.copy()
+    EC50_D1 = 1000*10**-9
+    k_off_D1 = (EC50_D1+12.4*10**-9)/(52*10**-9)
+    k_on_D1 = k_off_D1/EC50_D1
+    
+    
+    occ_D2 = space0.copy()
+    occ_D2_ph = space_ph.copy()
+    occ_D2_ph[:,:,:] = start_occ_D2
+    EC50_D2 = 7*10**-9
+    k_off_D2 = 0.2
+    k_on_D2 = k_off_D2/EC50_D2
+    
+    # DA release per vesicle
+    single_vesicle_vol = (4/3*np.pi*(0.025)**3) 
+    voxel_volume = (dx_dy)**3 # Volume of single voxel
+    single_vesicle_DA = 0.025 * Q/1000 # 0.025 M at Q = 1000
+    Q_eff = single_vesicle_vol/voxel_volume * single_vesicle_DA * 1/ECF
+    
+    
+    for i in tqdm(range(int(t/dt)-1)):
+        
+        # Add release events per time step
+        space_ph[release_sites[0,:][np.where(firing[i,:])],
+               release_sites[1,:][np.where(firing[i,:])],
+               release_sites[2,:][np.where(firing[i,:])]] += Q_eff
+        
+        # Add reverse transport
+        space_ph +=rev_trans[i]*dt
+        
+        # Apply gradient operator to simulate diffusion
+        space_ph, u = do_timestep_3D(space_ph, 
+                                      uptake_rate, Ds, dt, dx_dy, Kapp[i])
+        
+        # D2 Occ        
+        d_recep_occ = space_ph*k_on_D2*(1-occ_D2_ph) - k_off_D2*occ_D2_ph
+        occ_D2_ph = occ_D2_ph + dt*d_recep_occ
+        
+        # D1 Occ
+        d_recep_occ = space_ph*k_on_D1*(1-occ_D1_ph) - k_off_D1*occ_D1_ph
+        occ_D1_ph = occ_D1_ph + dt*d_recep_occ
+        
+        # Save snapshot at specified Hz
+        if i%int(Hz/dt) == 0:
+            space0[int(i/(Hz/dt)),:,:,:] = space_ph
+            
+            #Save receptor occupany
+            occ_D2[int(i/(Hz/dt)),:,:,:] = occ_D2_ph
+            
+            #Save receptor occupany
+            occ_D1[int(i/(Hz/dt)),:,:,:] = occ_D1_ph
+        
+    return space0, occ_D1, occ_D2
+
+
+def sim_dynamics_3D(space0, space_ph, release_sites, firing, var_list, 
+                 Q = 3000, uptake_rate = 4*10**-6, Km = 210*10**-9,
+                 Ds = 321.7237308146399, ECF = 0.21, start_occ_D2 = 0.3):
+    # print(uptake_rate)
+    # print(Q)
+    # Extract parameters
+    t = var_list[0]
+    dt = var_list[1]
+    dx_dy = var_list[2]
+    Hz = var_list[4]
+    
+    # Test for receptor occupany
+    # Affinities D2: https://www.ncbi.nlm.nih.gov/pmc/articles/PMC8071183/
+    occ_D1 = space0.copy()
+    occ_D1_ph = space_ph.copy()
+    EC50_D1 = 1000*10**-9
+    k_off_D1 = (EC50_D1+12.4*10**-9)/(52*10**-9)
+    k_on_D1 = k_off_D1/EC50_D1
+    
+    
+    occ_D2 = space0.copy()
+    occ_D2_ph = space_ph.copy()
+    occ_D2_ph[:,:,:] = start_occ_D2
+    EC50_D2 = 7*10**-9
+    k_off_D2 = 0.2
+    k_on_D2 = k_off_D2/EC50_D2
+    
+    
+    # DA release per vesicle
+    single_vesicle_vol = (4/3*np.pi*(0.025)**3) 
+    voxel_volume = (dx_dy)**3 # Volume of single voxel
+    single_vesicle_DA = 0.025 * Q/1000 # 0.025 M at Q = 1000
+    Q_eff = single_vesicle_vol/voxel_volume * single_vesicle_DA * 1/ECF
+    
+    
+    for i in tqdm(range(int(t/dt)-1)):
+        
+        # Add release events per time step
+        space_ph[release_sites[0,:][np.where(firing[i,:])],
+               release_sites[1,:][np.where(firing[i,:])],
+               release_sites[2,:][np.where(firing[i,:])]] += Q_eff
+        
+        # Apply gradient operator to simulate diffusion
+        space_ph, u = do_timestep_3D(space_ph, 
+                                      uptake_rate, Ds, dt, dx_dy, Km)
+        # D2 Occ        
+        d_recep_occ = space_ph*k_on_D2*(1-occ_D2_ph) - k_off_D2*occ_D2_ph
+        occ_D2_ph = occ_D2_ph + dt*d_recep_occ
+        
+        # D1 Occ
+        d_recep_occ = space_ph*k_on_D1*(1-occ_D1_ph) - k_off_D1*occ_D1_ph
+        occ_D1_ph = occ_D1_ph + dt*d_recep_occ
+        
+        # Save snapshot at specified Hz
+        if i%int(Hz/dt) == 0:
+            space0[int(i/(Hz/dt)),:,:,:] = space_ph
+            #Save receptor occupany
+            occ_D2[int(i/(Hz/dt)),:,:,:] = occ_D2_ph
+            
+            #Save receptor occupany
+            occ_D1[int(i/(Hz/dt)),:,:,:] = occ_D1_ph
+            
+        
+    return space0, occ_D1, occ_D2
+
+
+def do_timestep_3D(u0, uptake_rate, Ds, dt, dx_dy, Km):
+    u = u0.copy()
+    
+    # Propagate with forward-difference in time, central-difference in space
+    u = u0 + Ds * dt * (
+        (np.roll(u0, 1, axis = 0) - \
+         2*u0 + \
+         np.roll(u0, -1, axis = 0))  /dx_dy**2 
+              + \
+        (np.roll(u0, 1, axis = 1) - \
+         2*u0 + \
+         np.roll(u0, -1, axis = 1))  /dx_dy**2
+              + \
+        (np.roll(u0, 1, axis = 2) - \
+          2*u0 + \
+          np.roll(u0, -1, axis = 2))  /dx_dy**2)
+    
+    
+    # Simulate reuptake
+    u = u - dt*(uptake_rate*u)/(Km + u)
+    
+    u0 = u.copy()
+    return u0, u
+
+def impulse(t,k1,k2,tau,ts):
+    return np.exp(-(t+1)*(k1*tau+k2*ts))
+
+def amp_impulse(t,tau):
+    t = t*0.1
+    return (-1)**0/(2*t+1)*np.exp(-(2*t+1)**2*t/tau)
+
+def exp_decay(t,N0,time_constant):
+    return N0*np.exp(-time_constant*t)
